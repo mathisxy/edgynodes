a22defe03a128e9b278cb56f508f101a2436e7e6
mathisxy <mathis.siebert@gmail.com>
2026-01-26 14:13:58 +0000

    added claude

---
+++ edgynodes/llm/openai_claude.py
@@ -0,0 +1,6 @@
+from .openai import LLMNodeOpenAI
+
+class LLMNodeClaude(LLMNodeOpenAI):
+
+    def __init__(self, model: str, api_key: str, base_url: str = "https://api.anthropic.com/v1/", enable_streaming: bool = False) -> None:
+        super().__init__(model=model, api_key=api_key, base_url=base_url, enable_streaming=enable_streaming)
